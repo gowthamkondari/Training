feb836fba25eb5a19900464ecd9d9636f665f066
gowthamkondari <151503768+gowthamkondari@users.noreply.github.com>
2024-11-27 10:17:07 +0530

    Update and rename oops.txt to oops.m

--- oops.m
+++ oops.m
@@ -1,5 +1,6 @@
-OOPS CONCEPTS IN C++ : 
+OOPS CONCEPTS:
 The programming paradigm known as "Object-Oriented Programming" works with objects and classes. In a subsequent section of this article, we will talk about objects and classes. For the time being, we can state that object-oriented programming is a style of programming that, by emphasizing real-world elements (called objects), greatly simplifies programming.
+
 Object-Oriented Programming has 4 main Pillars. They are as follows.
 1.Inheritance
 2.Polymorphism
